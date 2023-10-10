19ad229ba026482051f44e0b9ad99a88876d4673
Bastian <bastian.p@outlook.com>
2023-10-10 17:13:40 -0300

    Cambiados png grande a webp

--- config/constants.js
+++ config/constants.js
@@ -99,7 +99,7 @@ const constants = {
       {
         DESCRIPTION:
           'A/B Tester de la plataforma. Desarrollo nuevas versiones de la página y comparo la antigua versión con la nueva para recolectar datos y así desplegar la que obtenga mejores resultados.',
-        IMAGE: 'falabella.png',
+        IMAGE: 'falabella.webp',
         LINK: 'https://www.falabella.com/falabella-cl/',
         NAME: 'Falabella',
         TECHNOLOGIES: [
@@ -117,7 +117,7 @@ const constants = {
         DESCRIPTION:
           'Página web de venta de bowls de ensalada que incluye un carrito de compras y una pasarela de pago construida mediante el servicio Webpay plus de Transbank. Las compras quedan registradas en una base de datos para consultar su estado.',
         GITHUB: 'https://github.com/BastianParedes/tusalad/',
-        IMAGE: 'tu-salad.png',
+        IMAGE: 'tu-salad.webp',
         LINK: '/tusalad',
         NAME: 'Tu Salad',
         TECHNOLOGIES: ['React.js', 'Next.js', 'TypeScript', 'WebPay']
@@ -126,7 +126,7 @@ const constants = {
         DESCRIPTION:
           'Es una página web que recibe las imágenes cargadas por el usuario, las configura a gusto y finalmente genera un archivo PDF que se guarda en el computador del mismo. Todo el proceso ocurre del lado del Front-end.',
         GITHUB: 'https://github.com/BastianParedes/pdf-constructor/',
-        IMAGE: 'pdf-constructor.png',
+        IMAGE: 'pdf-constructor.webp',
         LINK: '/pdf-constructor',
         NAME: 'PDF Constructor',
         TECHNOLOGIES: ['React.js', 'SortableJS', 'jsPDF']
@@ -135,7 +135,7 @@ const constants = {
         DESCRIPTION:
           'Aplicación web que pide al usuario un nombre y lo lleva a una sala donde todos los conectados a la página en ese momento pueden conversar al mismo tiempo.',
         GITHUB: 'https://github.com/BastianParedes/my-global-chat/',
-        IMAGE: 'my-global-chat.png',
+        IMAGE: 'my-global-chat.webp',
         LINK: '/my-global-chat',
         NAME: 'My Global Chat',
         TECHNOLOGIES: ['React.js', 'Socket.IO']
@@ -144,7 +144,7 @@ const constants = {
         DESCRIPTION:
           'CRUD que administra las contraseñas ingresadas por el ususario, las encripta y la muestra en el Front-End.',
         GITHUB: 'https://github.com/BastianParedes/passwords/',
-        IMAGE: 'passwords.png',
+        IMAGE: 'passwords.webp',
         LINK: '/passwords',
         NAME: 'Passwords',
         TECHNOLOGIES: ['Next.js', 'MySQL', 'Prisma', 'Bcrypt']
@@ -153,7 +153,7 @@ const constants = {
         DESCRIPTION:
           'Aplicación que genera el código QR correspondiente a la URL ingresada.',
         GITHUB: 'https://github.com/BastianParedes/qr-generator/',
-        IMAGE: 'qr-generator.png',
+        IMAGE: 'qr-generator.webp',
         LINK: '/qr-generator',
         NAME: 'QR generator',
         TECHNOLOGIES: ['QRcode', 'Next.js']
@@ -170,7 +170,7 @@ const constants = {
         DESCRIPTION:
           'Aplicación de escritorio que construye evaluaciones de seleción única de matemática para el nivel secundario de las escuelas chilenas.',
         GITHUB: 'https://github.com/BastianParedes/tstmaker/',
-        IMAGE: 'tstmaker.png',
+        IMAGE: 'tstmaker.webp',
         NAME: 'TstMaker',
         TECHNOLOGIES: ['Python3', 'Tkinter', 'MiKTeX']
       }
