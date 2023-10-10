75e13170e82da211be07711f80a21b43ef55539c
Bastian <bastian.p@outlook.com>
2023-10-10 18:07:55 -0300

    Quitados proyectos y puesto ba-tester

--- config/constants.js
+++ config/constants.js
@@ -6,9 +6,9 @@ const constants = {
     },
     DESCRIPTION_PART_1: 'Soy Bastián Paredes,',
     DESCRIPTION_PART_2:
-      'Un Front End Developer con experiencia construyendo sitios web con responsive design que conecten front-end y back-end. Me especializo en el uso de JavaScript, React.js y Next.js.',
+      'Un Front End Developer con experiencia construyendo sitios web con responsive design que conecten front-end y back-end. Me especializo en el uso de JavaScript, React.js y Next.js',
     DESCRIPTION_PART_3:
-      'Siempre busco mejorar mis habilidades desarrollando proyectos propios a la par de mi trabajo. Estoy constantemente estudiando acerca de las nuevas tecnologías para actualizar mi repertorio de herramientas para que los códigos de mis creaciones sean fáciles de leer, seguros y eficientes.',
+      'Siempre busco mejorar mis habilidades desarrollando proyectos propios a la par de mi trabajo. Estoy constantemente estudiando acerca de las nuevas tecnologías para actualizar mi repertorio de herramientas para que los códigos de mis creaciones sean fáciles de leer, seguros y eficientes',
     SECTION: {
       TITLE: 'About me'
     },
@@ -51,8 +51,16 @@ const constants = {
           SRC: 'prisma.svg'
         },
         {
+          NAME: 'Drizzle ORM',
+          SRC: 'drizzle-orm.png'
+        },
+        {
           NAME: 'MySql',
           SRC: 'mysql.png'
+        },
+        {
+          NAME: 'Postgresql',
+          SRC: 'postgresql.png'
         },
         {
           NAME: 'MongoDB',
@@ -61,6 +69,10 @@ const constants = {
         {
           NAME: 'Python 3',
           SRC: 'python3.png'
+        },
+        {
+          NAME: 'Docker',
+          SRC: 'docker.png'
         }
       ],
       TITLE: 'Skills'
@@ -98,7 +110,7 @@ const constants = {
     PROJECTS: [
       {
         DESCRIPTION:
-          'A/B Tester de la plataforma. Desarrollo nuevas versiones de la página y comparo la antigua versión con la nueva para recolectar datos y así desplegar la que obtenga mejores resultados.',
+          'A/B Tester de la plataforma. Desarrollo nuevas versiones de la página y comparo la antigua versión con la nueva para recolectar datos y así desplegar la que obtenga mejores resultados',
         IMAGE: 'falabella.webp',
         LINK: 'https://www.falabella.com/falabella-cl/',
         NAME: 'Falabella',
@@ -115,16 +127,25 @@ const constants = {
       },
       {
         DESCRIPTION:
-          'Página web de venta de bowls de ensalada que incluye un carrito de compras y una pasarela de pago construida mediante el servicio Webpay plus de Transbank. Las compras quedan registradas en una base de datos para consultar su estado.',
-        GITHUB: 'https://github.com/BastianParedes/tusalad/',
-        IMAGE: 'tu-salad.webp',
-        LINK: '/tusalad',
-        NAME: 'Tu Salad',
-        TECHNOLOGIES: ['React.js', 'Next.js', 'TypeScript', 'WebPay']
+          'Aplicación web que permite implementar pruebas A/B en cualquier página web mediante la recopilación de los tests por parte de los desarrolladores y la disponibilidad de un paquete a través de un endpoint',
+        IMAGE: 'ba-tester.webp',
+        LINK: 'https://github.com/BastianParedes/ba-tester/',
+        NAME: 'BA-Tester',
+        TECHNOLOGIES: [
+          'HTML',
+          'CSS',
+          'JavaScript',
+          'React.js',
+          'Next.js',
+          'TypeScript',
+          'Postgresql',
+          'Graphql',
+          'DrizzleOrm'
+        ]
       },
       {
         DESCRIPTION:
-          'Es una página web que recibe las imágenes cargadas por el usuario, las configura a gusto y finalmente genera un archivo PDF que se guarda en el computador del mismo. Todo el proceso ocurre del lado del Front-end.',
+          'Es una página web que recibe las imágenes cargadas por el usuario, las configura a gusto y finalmente genera un archivo PDF que se guarda en el computador del mismo. Todo el proceso ocurre del lado del Front-end',
         GITHUB: 'https://github.com/BastianParedes/pdf-constructor/',
         IMAGE: 'pdf-constructor.webp',
         LINK: '/pdf-constructor',
@@ -133,46 +154,20 @@ const constants = {
       },
       {
         DESCRIPTION:
-          'Aplicación web que pide al usuario un nombre y lo lleva a una sala donde todos los conectados a la página en ese momento pueden conversar al mismo tiempo.',
-        GITHUB: 'https://github.com/BastianParedes/my-global-chat/',
-        IMAGE: 'my-global-chat.webp',
-        LINK: '/my-global-chat',
-        NAME: 'My Global Chat',
-        TECHNOLOGIES: ['React.js', 'Socket.IO']
+          'Aplicación de escritorio que construye evaluaciones de seleción única de matemática para el nivel secundario de las escuelas chilenas',
+        GITHUB: 'https://github.com/BastianParedes/tstmaker/',
+        IMAGE: 'tstmaker.webp',
+        NAME: 'TstMaker',
+        TECHNOLOGIES: ['Python3', 'Tkinter', 'MiKTeX']
       },
       {
         DESCRIPTION:
-          'CRUD que administra las contraseñas ingresadas por el ususario, las encripta y la muestra en el Front-End.',
-        GITHUB: 'https://github.com/BastianParedes/passwords/',
-        IMAGE: 'passwords.webp',
-        LINK: '/passwords',
-        NAME: 'Passwords',
-        TECHNOLOGIES: ['Next.js', 'MySQL', 'Prisma', 'Bcrypt']
-      },
-      {
-        DESCRIPTION:
-          'Aplicación que genera el código QR correspondiente a la URL ingresada.',
+          'Aplicación que genera el código QR correspondiente a la URL ingresada',
         GITHUB: 'https://github.com/BastianParedes/qr-generator/',
         IMAGE: 'qr-generator.webp',
         LINK: '/qr-generator',
         NAME: 'QR generator',
         TECHNOLOGIES: ['QRcode', 'Next.js']
-      },
-      {
-        DESCRIPTION:
-          'Aplicación que muestra el clima actual en la ubicación ingresada.',
-        GITHUB: 'https://github.com/BastianParedes/weather/',
-        LINK: '/weather',
-        NAME: 'Weather',
-        TECHNOLOGIES: ['Google Maps API', 'openWeather API', 'Next.js']
-      },
-      {
-        DESCRIPTION:
-          'Aplicación de escritorio que construye evaluaciones de seleción única de matemática para el nivel secundario de las escuelas chilenas.',
-        GITHUB: 'https://github.com/BastianParedes/tstmaker/',
-        IMAGE: 'tstmaker.webp',
-        NAME: 'TstMaker',
-        TECHNOLOGIES: ['Python3', 'Tkinter', 'MiKTeX']
       }
     ]
   }
