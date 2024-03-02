f6817ac44b143d8a2d6c5b51db358fbb64f7370c
Bastián <bparedesp@falabella.cl>
2024-03-02 19:20:14 -0300

    Cambiados skills

--- src/app/components/About/skills/index.ts
+++ src/app/components/About/skills/index.ts
@@ -1,95 +1,117 @@
 import css from './css.png';
 import docker from './docker.png';
-import drizzle from './drizzle-orm.png';
+import express from './express.png';
+import fastify from './fastify.png';
+// import drizzle from './drizzle-orm.png';
 import git from './git.png';
-import graphql from './graphql.png';
+// import graphql from './graphql.png';
 import html from './html5.png';
-import java from './java.png';
+// import java from './java.png';
 import javascript from './javascript.png';
-import mongodb from './mongodb.png';
+// import mongodb from './mongodb.png';
 import mysql from './mysql.png';
 import next from './next.js.png';
 import node from './nodejs.png';
 import postgre from './postgresql.png';
-import prisma from './prisma.png';
+// import prisma from './prisma.png';
 import python from './python3.png';
 import react from './react.png';
-import sass from './sass.png';
+import svelte from './svelte.png';
+// import sass from './sass.png';
 import typescript from './typescript.png';
 
 const skills = [
-  {
-    image: css,
-    name: 'CSS'
-  },
-  {
-    image: docker,
-    name: 'Docker'
-  },
-  {
-    image: drizzle,
-    name: 'Drizzle ORM'
-  },
-  {
-    image: git,
-    name: 'Git'
-  },
-  {
-    image: graphql,
-    name: 'GraphQL'
-  },
   {
     image: html,
     name: 'HTML'
   },
   {
-    image: java,
-    name: 'Java'
+    image: css,
+    name: 'CSS'
   },
   {
     image: javascript,
     name: 'JavaScript'
   },
   {
-    image: mongodb,
-    name: 'Mongo DB'
-  },
-  {
-    image: mysql,
-    name: 'MySql'
-  },
-  {
-    image: next,
-    name: 'NextJS'
-  },
-  {
-    image: node,
-    name: 'Node'
-  },
-  {
-    image: postgre,
-    name: 'Postgre SQL'
-  },
-  {
-    image: prisma,
-    name: 'Prisma'
+    image: typescript,
+    name: 'TypeScript'
   },
   {
     image: python,
     name: 'Python'
+  },
+  /* {
+    image: java,
+    name: 'Java'
+  }, */
+  {
+    image: node,
+    name: 'NodeJS'
   },
   {
     image: react,
     name: 'React'
   },
   {
-    image: sass,
-    name: 'Sass'
+    image: react,
+    name: 'React Native'
   },
   {
-    image: typescript,
-    name: 'TypeScript'
+    image: svelte,
+    name: 'Svelte'
+  },
+  {
+    image: next,
+    name: 'NextJS'
+  },
+  {
+    image: express,
+    name: 'Express'
+  },
+  {
+    image: fastify,
+    name: 'Fastify'
+  },
+  {
+    image: mysql,
+    name: 'MySql'
+  },
+  {
+    image: postgre,
+    name: 'Postgre SQL'
+  },
+  {
+    image: docker,
+    name: 'Docker'
+  },
+  {
+    image: git,
+    name: 'Git'
   }
+  /* {
+    image: prisma,
+    name: 'Prisma'
+  }, */
+  /* {
+    image: drizzle,
+    name: 'Drizzle ORM'
+  }, */
+
+  /* {
+    image: graphql,
+    name: 'GraphQL'
+  }, */
+
+  /* {
+    image: mongodb,
+    name: 'Mongo DB'
+  }, */
+
+  /* {
+    image: sass,
+    name: 'Sass'
+  }, */
 ];
 
 export default skills;
